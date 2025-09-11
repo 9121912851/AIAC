4ceab3560afe5f682f1d70691e8f2e52334bce40
9121912851 <2403a51023@sru.edu.in>
2025-09-11 10:48:39 +0530

    Add files via upload

---
+++ lab 11/task5.py
@@ -0,0 +1,61 @@
+from collections import deque
+
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_edge(self, src, dest):
+        if src not in self.adj_list:
+            self.adj_list[src] = []
+        if dest not in self.adj_list:
+            self.adj_list[dest] = []
+        self.adj_list[src].append(dest)
+        self.adj_list[dest].append(src)
+
+    def bfs(self, start):
+        visited = set()
+        queue = deque([start])
+        result = []
+        while queue:
+            node = queue.popleft()
+            if node not in visited:
+                visited.add(node)
+                result.append(node)
+                queue.extend(neighbor for neighbor in self.adj_list[node] if neighbor not in visited)
+        return result
+
+    def dfs_recursive(self, start):
+        result = []
+        visited = set()
+        def dfs(node):
+            if node not in visited:
+                visited.add(node)
+                result.append(node)
+                for neighbor in self.adj_list[node]:
+                    dfs(neighbor)
+        dfs(start)
+        return result
+
+    def dfs_iterative(self, start):
+        visited = set()
+        stack = [start]
+        result = []
+        while stack:
+            node = stack.pop()
+            if node not in visited:
+                visited.add(node)
+                result.append(node)
+                stack.extend(reversed(self.adj_list[node]))
+        return result
+
+g = Graph()
+g.add_edge('A', 'B')
+g.add_edge('A', 'C')
+g.add_edge('B', 'D')
+g.add_edge('C', 'E')
+g.add_edge('D', 'F')
+g.add_edge('E', 'F')
+
+print("BFS:", g.bfs('A'))
+print("DFS Recursive:", g.dfs_recursive('A'))
+print("DFS Iterative:", g.dfs_iterative('A'))
